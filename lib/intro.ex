1d51614de91a1255685d667acf16ad8ca6298fe1
majs <myelovell@hotmail.com>
2021-08-26 19:15:07 +0200

    Started task with elixir

--- lib/intro.ex
+++ lib/intro.ex
@@ -1,5 +1,106 @@
 defmodule Intro do
-  def hello do
-    :world
+  # uppgift 1
+    def smallest(n1,n2) when n1 < n2, do: n1
+    def smallest(_n1,n2), do: n2
+
+  # uppgift 2
+    def largest(n1,n2,n3) when n1 > n2 and n1 > n3, do: n1
+    def largest(n1,n2,n3) when n2 > n1 and n2 > n3, do: n2
+    def largest(_n1,_n2,n3), do: n3
+
+  # uppgift 3
+    def smallest(n1,n2,n3,n4), do: smallest(smallest(n3,n4),smallest(n1,n2))
+
+  # uppgift 4
+    def ticketPrice(age) when age < 18, do: "10"
+    def ticketPrice(age) when age > 64, do: "15"
+    def ticketPrice(age) when age >= 18 and age <= 64, do: "20"
+
+  # uppgift 5
+    def nextNumber(num), do: num + 1
+
+  # uppgift 6
+    def factorial(num) do
+      cond do
+        num == 1 ->
+        1
+        num !== 1 ->
+        num*factorial(num - 1)
+      end
+    end
+
+  # uppgift 7
+    def fibbonachi(num) do
+      cond do
+        num == 0 ->
+          0
+        num == 1 ->
+          1
+        true ->
+          fibbonachi(num - 1) + fibbonachi(num - 2)
+      end
+    end
+
+  # uppgift 8
+    def isEmpty(list) do
+      [head | _tail] = list
+      cond do
+        head == nil ->
+          true
+        head !== nil ->
+          false
+      end
+    end
+
+  # uppgift 9
+    def firstOf(list) do
+      [head | _tail] = list
+      head
+    end
+
+  # uppgift 10
+  # def lastOf(list) do
+  #   [head | tail] = list
+  #   cond do
+  #     tail !== [nil] ->
+  #       lastOf(tail)
+  #     tail == [nil] ->
+  #       head
+  #   end
+  #   head
+  # end
+
+  def lastOf([head | tail]) when tail == [], do: head
+  def lastOf([_head | tail]), do: lastOf(tail)
+
+
+  # uppgift 11
+    def prepend(int, list), do: [int | list]
+
+  # uppgift 12
+    def append(int, []), do: [int]
+    def append(int, [head | tail]), do: [head | append(int, tail)]
+
+
+    #uppgift 13
+    def len(list, acc \\ 0 )
+    def len([], acc), do: acc
+    def len([_head | tail], acc), do: len(tail, acc + 1)
+
+    #uppgift 14
+    def summary(list, acc \\ 0 )
+    def summary([], acc), do: acc
+    def summary([head | tail], acc), do: summary(tail, acc + head)
+
+    #uppgift 15
+    def ave(list), do: summary(list) / len(list)
+
+    #uppgift 16
+    def concat(l1, l2) when l1 == [], do: l2
+    def concat([head | tail], l2), do: concat(tail, append(head, l2))
+
+    #uppgift 17
+    def startWith("","")
+
+
   end
-end
--- test/intro_test.exs
+++ test/intro_test.exs
@@ -1,7 +1,154 @@
 defmodule IntroTest do
   use ExUnit.Case
+  # uppgift 1
+    # test "smallest of two when smallest is first" do
+    #   assert Intro.smallest(1,2) == 1
+    # end
 
-  test "greets the world" do
-    assert Intro.hello() == :world
+    # test "smallest of two when smallest is last" do
+    #   assert Intro.smallest(3,2) == 2
+    # end
+
+    # test "smallest of two of equal size" do
+    #   assert Intro.smallest(1337,1337) == 1337
+    # end
+
+
+  # uppgift 2
+    # test "largest of three when largest is first" do
+    #   assert Intro.largest(3,2,1) == 3
+    # end
+
+    # test "largest of three when largest is in the middle" do
+    #   assert Intro.largest(1,4,2) == 4
+    # end
+
+    # test "largest of three when largest is last" do
+    #   assert Intro.largest(3,2,1337) == 1337
+    # end
+
+    # test "largest of three of equal size" do
+    #   assert Intro.largest(6,6,6) == 6
+    # end
+
+
+  # uppgift 3
+    # test "smallest of two when smallest is first" do
+    #     assert Intro.smallest(1,2,3,4) == 1
+    #   end
+
+    #   test "smallest of two when smallest is last" do
+    #     assert Intro.smallest(4,3,2,1) == 1
+    #   end
+
+    #   test "smallest of two of equal size" do
+    #     assert Intro.smallest(1337,1337,1337,1337) == 1337
+    #   end
+
+  # uppgift 4
+    # test "ticketPrice under 18" do
+    #   assert Intro.ticketPrice(12) == 10
+    # end
+
+    # test "ticketPrice over 18 under 64" do
+    #   assert Intro.ticketPrice(19) == 20
+    # end
+
+    # test "ticketPrice when 18" do
+    #   assert Intro.ticketPrice(18) == 20
+    # end
+
+    # test "ticketPrice under 18" do
+    #   assert Intro.ticketPrice(64) == 20
+    # end
+
+    # test "ticketPrice over 64" do
+    #   assert Intro.ticketPrice(65) == 15
+    # end
+
+  # uppgift 5
+    # test "nextNum after 5" do
+    #   assert Intro.nextNumber(5) == 6
+    # end
+
+    # test "nextNum after -40" do
+    #   assert Intro.nextNumber(-40) == -39
+    # end
+
+    # test "nextNum after 200" do
+    #   assert Intro.nextNumber(200) == 201
+    # end
+
+  # uppgift 6
+    # test "factorial of 2" do
+    #   assert Intro.factorial(2) == 2
+    # end
+
+    # test "factorial of 5" do
+    #   assert Intro.factorial(5) == 120
+    # end
+
+    # test "factorial of 8" do
+    #   assert Intro.factorial(8) == 40320
+    # end
+
+# uppgift 7
+      # test "fib num 5" do
+      #   assert Intro.fibbonachi(5) == 5
+      # end
+
+      # test "fib num 6" do
+      #   assert Intro.fibbonachi(6) == 8
+      # end
+
+      # test "fib num 1" do
+      #   assert Intro.fibbonachi(1) == 1
+      # end
+
+# uppgift 8
+  # test "list is empty" do
+  #   assert Intro.isEmpty([1,2]) == false
+  # end
+
+  # test "list is not empty" do
+  #   assert Intro.isEmpty([nil]) == true
+  # end
+
+# uppgift 9
+  # test "list head 1" do
+  #   assert Intro.firstOf([1, 4, 5, 7, 7, 1 ,2]) == 1
+  # end
+
+  # test "list head empty" do
+  #   assert Intro.firstOf([nil]) == nil
+  # end
+
+  # test "list head hello" do
+  #   assert Intro.firstOf(["hello", 5]) == "hello"
+  # end
+
+# uppgift 10
+  # test "list head 1" do
+  #   assert Intro.lastOf([1, 4, 5, 7, 7, 1 ,2]) == 2
+  # end
+
+  # test "list head hello" do
+  #   assert Intro.lastOf(["hello", 5, "world"]) == "world"
+  # end
+
+# uppgift 11
+  #test "int = 5 list = [2,3]" do
+    #assert Intro.prepend(5, [2,3]) == [5,2,3]
+  #end
+
+  #test "append" do
+  #assert Intro.append(4, [1,2,3]) == [1,2,3,4]
+  #end
+
+  test "length" do
+    assert Intro.len([1,2,5,6,1]) == 5
   end
+
+
+
 end
